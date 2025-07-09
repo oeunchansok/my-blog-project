b34dd2b62f9121168e28229ab1850449c74d87c0
Chansok <chansok.o@aditi.asia>
2025-07-09 09:59:32 +0700

    update

--- app.vue
+++ app.vue
@@ -3,3 +3,21 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<script setup>
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-91LV7TYNYN',
+      async: true
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-91LV7TYNYN');
+      `
+    }
+  ]
+})
+</script>
